98b656a3915e2f2f8d9751a6d1a89174ff44e8d5
Hector Alfaro <hectorsector@github.com>
2021-02-03 14:29:01 -0500

    add error handling

--- main.js
+++ main.js
@@ -1,44 +1,46 @@
-const github = require('@actions/github')
 const core = require('@actions/core')
 const yaml = require('js-yaml')
 const fs = require('fs')
 
-async function run () {}
+async function run () {
+  try {
+    const files = core.getInput('files').split(',')
+    //   TODO: verify the proper files were passed and learner didn't tamper with grading.yml
 
-try {
-  const token = core.getInput('github-token')
-  const files = core.getInput('files').split(',')
-  //   TODO: verify the proper files were passed and learner didn't tamper with grading.yml
+    const answers = {
+      'stale-daily': '0 0 * * *',
+      'stale-weekly': '0 0 * * MON',
+      'stale-monthly': '0 0 1 * *'
+    }
 
-  const answers = {
-    'stale-daily': '0 0 * * *',
-    'stale-weekly': '0 0 * * MON',
-    'stale-monthly': '0 0 1 * *'
-  }
+    files.forEach((file) => {
+      const filename = file.replace('.yml', '')
 
-  files.forEach((file) => {
-    const filename = file.replace('.yml', '')
-
-    const doc = yaml.load(
-      fs.readFileSync(
+      const doc = yaml.load(
+        fs.readFileSync(
         `${process.env.GITHUB_WORKSPACE}/.github/workflows/${file}`,
         'utf8'
+        )
       )
-    )
-    // TODO: if desired keys dont' exist prevent failure but provide feedback
-    if (answers[filename].trim() === doc.on.schedule[0].cron.trim()) {
-      console.log('Equal... you pass')
-    } else {
-      console.log('you suck')
-    }
-  })
-} catch (error) {
-  core.setFailed(error)
+      // TODO: if desired keys dont' exist prevent failure but provide feedback
+      if (answers[filename].trim() === doc.on.schedule[0].cron.trim()) {
+        console.log(`Results for ${filename}: correct`)
+      } else {
+        console.log(`Results for ${filename}: incorrect`)
+        core.setFailed(
+          `File ${filename} is incorrect, see the troubleshooting step for help.`
+        )
+        core.setOutput('report', {
+          type: 'issue',
+          level: 'warning',
+          msg:
+          `Expected ${filename} to contain the cron syntax ${answers[filename]}, got ${doc.on.schedule[0].cron.trim()}`
+        })
+      }
+    })
+  } catch (error) {
+    core.setFailed(error)
+  }
 }
 
-// Get document, or throw exception on error
-try {
-  run()
-} catch (e) {
-  console.log(e)
-}
+run()
