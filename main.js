1eb020f9449ca0fb6b51349e02815bbccb02388a
Hector Alfaro <hectorsector@github.com>
2021-02-03 14:00:36 -0500

    lint

--- main.js
+++ main.js
@@ -1,45 +1,44 @@
-const github = require("@actions/github");
-const core = require("@actions/core");
-const yaml = require("js-yaml");
-const fs = require("fs");
+const github = require('@actions/github')
+const core = require('@actions/core')
+const yaml = require('js-yaml')
+const fs = require('fs')
 
-async function run() {}
+async function run () {}
 
 try {
-  token = core.getInput("github-token");
-  files = core.getInput("files").split(",");
+  const token = core.getInput('github-token')
+  const files = core.getInput('files').split(',')
   //   TODO: verify the proper files were passed and learner didn't tamper with grading.yml
 
   const answers = {
-    "stale-daily": "0 0 * * *",
-    "stale-weekly": "0 0 * * MON",
-    "stale-monthly": "0 0 1 * *",
-  };
+    'stale-daily': '0 0 * * *',
+    'stale-weekly': '0 0 * * MON',
+    'stale-monthly': '0 0 1 * *'
+  }
 
   files.forEach((file) => {
-    const filename = file.replace(".yml", "");
+    const filename = file.replace('.yml', '')
 
     const doc = yaml.load(
       fs.readFileSync(
         `${process.env.GITHUB_WORKSPACE}/.github/workflows/${file}`,
-        "utf8"
+        'utf8'
       )
-    );
+    )
     // TODO: if desired keys dont' exist prevent failure but provide feedback
     if (answers[filename].trim() === doc.on.schedule[0].cron.trim()) {
-      console.log("Equal... you pass");
+      console.log('Equal... you pass')
     } else {
-      console.log("you suck");
+      console.log('you suck')
     }
-  });
+  })
 } catch (error) {
-  core.setFailed(error);
+  core.setFailed(error)
 }
-
-run();
 
 // Get document, or throw exception on error
 try {
+  run()
 } catch (e) {
-  console.log(e);
+  console.log(e)
 }
